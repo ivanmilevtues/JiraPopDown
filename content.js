3effeb49ad318f3062488aa262a6015bcc7ed40f
ivanmilevtues <ivanmilevtues@gmail.com>
2019-04-09 00:21:20 +0300

    Added basic removal for pop-ups

---
+++ content.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.text === 'newTab') {
+        clickIfDefined($("#aui-flag-container > div:nth-child(1) > div > span"));
+        clickIfDefined($("#nps-acknowledgement-accept-button"));
+        clickIfDefined($("#inline-dialog-post-setup-announcement-database-setup > div.aui-inline-dialog-contents.contents > div > p.post-setup-discovery-buttons > a.aui-button.post-setup-action.post-setup-action-js.post-setup-discovery-close-js"));
+        clickIfDefined($("#inline-dialog-jira-help-tip-15539905126670 > div.aui-inline-dialog-contents > form > button"));
+    }
+});
+
+function clickIfDefined(element) {
+    if(element !== undefined) {
+        element.click();
+    }
+}
